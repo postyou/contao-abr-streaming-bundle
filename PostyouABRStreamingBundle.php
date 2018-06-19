c14a71ad493485b91d060587f2b7fb12a4ab9130
markus <markus.nestmann@postyou.de>
2018-06-19 14:56:55 +0200

    add required file structure

---
+++ PostyouABRStreamingBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * Extension for Contao Open Source CMS (contao.org)
+ *
+ * Copyright (c) 2018-2018 POSTYOU
+ *
+ * @package
+ * @author  Markus Nestmann
+ * @link    http://www.postyou.de
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+namespace Postyou\ABRStreamingBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PostyouABRStreamingBundleBundle extends Bundle
+{
+}
