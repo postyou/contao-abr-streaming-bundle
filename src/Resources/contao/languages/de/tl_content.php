837b869ed36e27f4838a5411ddd997fc37730998
markus <markus.nestmann@gmx.de>
2018-09-05 08:31:33 +0200

    fix array name and add new translation

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -5,8 +5,8 @@
  /**
   * Fields
   */
- $GLOBALS['TL_LANG']['tl_content']['directory'] = array('Ordner', 'Bitte geben Sie den Ordner an.');
+ $GLOBALS['TL_LANG']['tl_content']['abrs_playerSRC'] = array('Video-Dateien', 'Hier können Sie die Video-Datei bzw. – wenn Sie verschiedene Codecs verwenden – die Video-Dateien hinzufügen.');
  /**
   * Legends
   */
- $GLOBALS['TL_LANG']['tl_content']['sources'] = 'Quellen';
+ $GLOBALS['TL_LANG']['tl_content']['sources'] = 'Dateien';
