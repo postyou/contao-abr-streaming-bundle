0a6f533adcb59f3cabb83bfd4e2596fa2303ef04
MarkejN <markus.nestmann@gmx.de>
2020-01-15 11:13:26 +0100

    start implementing new dash version

--- src/Resources/public/js/qualityselection.js
+++ src/Resources/public/js/qualityselection.js
@@ -88,12 +88,12 @@ Object.assign(MediaElementPlayer.prototype, {
 
 		for (let i = 0, total = sources.length; i < total; i++) {
 			const src = sources[i];
-			if (src.mediaType === "video") {
-				if (src.qualityIndex === dashPlayer.getQualityFor("video") && !dashPlayer.getAutoSwitchQualityFor("video")) var isCurrent = true;
+			if (src.mediaType === "video") {				
+				if (src.qualityIndex === dashPlayer.getQualityFor("video") && !dashPlayer.getSettings().streaming.abr.autoSwitchBitrate.video) var isCurrent = true;
 				t.addQualityButton(src.height + "p", src.qualityIndex, isCurrent);
 			}
 		}
-		t.addQualityButton("Automatisch", "auto", dashPlayer.getAutoSwitchQualityFor("video"));
+		t.addQualityButton("Automatisch", "auto", dashPlayer.getSettings().streaming.abr.autoSwitchBitrate.video);
 		return 0;
 	},
 
@@ -185,14 +185,38 @@ Object.assign(MediaElementPlayer.prototype, {
 
 		if (selectedSrc === "auto") {
 			dashPlayer.setFastSwitchEnabled(false);
-			dashPlayer.setAutoSwitchQualityFor('video', true);
+			dashPlayer.updateSettings({
+				'streaming': {
+					'abr': {
+						'autoSwitchBitrate': {
+							'video': true
+						}
+					}
+				}
+			});
 		} else if (dashPlayer.getQualityFor('video') !== selectedSrc) {
-			dashPlayer.setAutoSwitchQualityFor('video', false);
+			dashPlayer.updateSettings({
+				'streaming': {
+					'abr': {
+						'autoSwitchBitrate': {
+							'video': false
+						}
+					}
+				}
+			});
 			dashPlayer.setFastSwitchEnabled(true);
 			dashPlayer.setQualityFor('video', selectedSrc);
 		} else {
 			dashPlayer.setFastSwitchEnabled(true);
-			dashPlayer.setAutoSwitchQualityFor('video', false);
+			dashPlayer.updateSettings({
+				'streaming': {
+					'abr': {
+						'autoSwitchBitrate': {
+							'video': false
+						}
+					}
+				}
+			});
 		}
 		t.updateQualityButton(dashPlayer.getQualityFor('video'), classPrefix);
 	},
