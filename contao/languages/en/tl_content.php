34938b618d495a2153ffe1011214287fbd95186a
MarkejN <markus.nestmann@outlook.com>
2024-12-19 09:29:47 +0100

    Add en translation

---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of postyou/contao-abr-streaming-bundle.
+ *
+ * (c) POSTYOU Werbeagentur
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_LANG']['tl_content']['videojs_legend'] = 'Video.js settings';
+$GLOBALS['TL_LANG']['tl_content']['useVideoJs'] = ['Use Video.js', ''];
+$GLOBALS['TL_LANG']['tl_content']['videoJsSetup'] = ['Setup-JSON', 'Here you can add Video.js options.'];
